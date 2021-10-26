ce5576dc7342e8d9cd183ec15ed3cba21ac57231
Owner <Askndr312@gmail.com>
2021-10-26 13:25:01 +0300

    امثلة للبرويكت

--- app/src/main/java/com/example/osamataskmanger/MainActivity.java
+++ app/src/main/java/com/example/osamataskmanger/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,7 +14,7 @@ import android.widget.SearchView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
     private SearchView svTask;
     private ListView lstvAllTask;
     private FloatingActionButton faBtn;
@@ -51,14 +52,20 @@ public class MainActivity extends AppCompatActivity {
         if(item.getItemId()==R.id.itmSignout)
         {
             AlertDialog.Builder builder=new AlertDialog.Builder(this);
-            //
             builder.setMessage("Are you sure?");
-            //
             builder.setCancelable(true);
-            //
-            //builder.setPositiveButton("Yes",this);
-            //
+            builder.setPositiveButton("Yes",this);
+            builder.setNegativeButton("No",this);
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+
         }
         return true;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
     }
 }
